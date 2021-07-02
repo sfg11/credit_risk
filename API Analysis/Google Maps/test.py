610c70e5922b3b340ae770c577bae96142e24da8
udaiparvatha <udaiparvatha@gmail.com>
2021-07-02 17:22:14 -0400

    updated

--- API Analysis/Google Maps/test.py
+++
@@ -1,5 +0,0 @@
-import googlemaps
-Key ='AIzaSyCbE5u5-3CmLSKbMl-BOME5SSsrwzEvGFg'
-gmaps = googlemaps.Client(key=Key)
-details = gmaps.places_autocomplete_query('Fortinos near Oakville, ON, Canada')[0]['description']
-print(details)
